e3749607418c62284be69c1bb7362c5266592309
kishlay <kishlay141@gmail.com>
2025-04-09 13:29:15 +0530

    connected mysql databade with php

---
+++ php/db_connect.php
@@ -0,0 +1,17 @@
+<?php
+include 'config.php';
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'lms';
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the database!";
+?>
+ 
