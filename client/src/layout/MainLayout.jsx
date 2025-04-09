68b55c175d66fa48a8c25040fcaaee9252b1d308
kishlay <kishlay141@gmail.com>
2025-04-09 16:16:11 +0530

    updated the mainlayoutjsx so that the navbar doens't get visible at all routes

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,16 +1,24 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import Navbar from "@/components/Navbar";
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
-  return (
-    <div className='flex flex-col min-h-screen'>
-        <Navbar/>
-        <div className='flex-1 mt-16'>
-            <Outlet/>
-        </div>
-    </div>
-  )
-}
+  const location = useLocation();
 
-export default MainLayout
+  const hideNavbarRoutes = ["/", "/teacher-registration"];
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+      <div
+        className={`flex-1 ${
+          !hideNavbarRoutes.includes(location.pathname) ? "mt-16" : ""
+        }`}
+      >
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
