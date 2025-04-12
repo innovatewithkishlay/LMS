2c9a9c83933cc55bfba8e0a4400ac29be207bb5a
kishlay <kishlay141@gmail.com>
2025-04-12 11:52:56 +0530

    corrected the error of chatbot

--- php/chatbot.php
+++ php/chatbot.php
@@ -1,7 +1,15 @@
 <?php
+header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 header("Content-Type: application/json");
 
 require_once "config.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit;
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $input = json_decode(file_get_contents("php://input"), true);
